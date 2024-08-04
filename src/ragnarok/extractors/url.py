b19050e73ec4e1fb73c2252b0f750a43e1a633dc
Harsh Nigam <nigharsh@gmail.com>
2024-08-04 01:43:41 -0700

    crawl fixes

--- src/ragnarok/extractors/url.py
+++ src/ragnarok/extractors/url.py
@@ -5,13 +5,13 @@ from ..crawlers.base import BaseCrawler
 class URLExtractor(BaseExtractor):
     def __init__(self, config: dict):
         super().__init__(config)
-        self.depth = config.get("depth", 0)
 
-    def extract(self, source: Any, crawler: BaseCrawler, *args, **kwargs) -> ExtractorOutput:
-        if self.depth < 0:
+    def extract(self, source: Any, crawler: BaseCrawler, **kwargs) -> ExtractorOutput:
+        depth = kwargs.get("depth", 0)
+        if depth < 0:
             raise ValueError("Depth must be greater than or equal to 0")
         else:
-            return crawler.crawl(source, depth=self.depth)
+            return crawler.crawl(source, depth=depth)
 
     @classmethod
     def from_config(cls, config: dict) -> "URLExtractor":
